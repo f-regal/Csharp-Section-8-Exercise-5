1ecb0292beeb306b28e5ea7b110a46962832567e
Faysal Regal <faysalregal@gmail.com>
2022-08-22 18:29:02 +0100

    started exercise 5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//    Exercise 5
+//    Write a program and ask the user to enter an English word.
+//    Count the number of vowels (a, e, o, u, i) in the word.
+//    So, if the user enters "inadequate", the program should display 6 on the console.
+
+namespace CsharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an English word");
+            var userInput = Console.ReadLine();
+            var splitInput = userInput.Split(' ');
+            var counter = 0;
+
+            for(var i = 0; i < splitInput.Length; i++)
+            {
+                if (splitInput[i])
+                {
+                    counter++;
+                }
+            }
+
+        }
+    }
+}
